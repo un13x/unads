d5bf9baf9de9b64bdf93c965bef5eab4e6fb7508
Uni Marhaidha <96172453+un13x@users.noreply.github.com>
2022-05-06 03:28:45 +0800

    Update unads.js

--- unads.js
+++ unads.js
@@ -1,9 +1,9 @@
 var dataUrl = [
-'https://plumbingcurlproximity.com/eivf1rjke?key=83661346f4f768305057daed32a4bb70',
-'https://plumbingcurlproximity.com/hv884gq9?key=ddb9d0390b12ecd3ccebc8fc78a1413d',
-'https://plumbingcurlproximity.com/wxcenvshdx?key=7c6ca30775fee8458ee180571fa36a1d',
-'https://plumbingcurlproximity.com/xwev255ir?key=63638ace446b2ff7705f0710d4e04295',
-'https://plumbingcurlproximity.com/wcz24tk6?key=8c28f7643716d3c138a3b91c607f87ae'
+'https://hosieryplum.com/xwev255ir?key=63638ace446b2ff7705f0710d4e04295',
+'https://hosieryplum.com/wcz24tk6?key=8c28f7643716d3c138a3b91c607f87ae',
+'https://hosieryplum.com/eivf1rjke?key=83661346f4f768305057daed32a4bb70',
+'https://hosieryplum.com/wxcenvshdx?key=7c6ca30775fee8458ee180571fa36a1d',
+'https://hosieryplum.com/hv884gq9?key=ddb9d0390b12ecd3ccebc8fc78a1413d'
 ];
 var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
 window.location = randomItem;
